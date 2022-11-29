00d0066647c436656003a4b8a02b3d5937f49764
H-Show <95389843+H-Show@users.noreply.github.com>
2022-11-29 16:31:38 +0900

    1129Missile

---
+++ Ta-mya/Assets/MyFolder/Honda/Missile/Homing.cs
@@ -0,0 +1,115 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Homing : MonoBehaviour
+{
+    [SerializeField]
+    Transform target;
+    [SerializeField, Min(0)]
+    float time = 2.5f;
+    [SerializeField]
+    float lifeTime = 3;
+    [SerializeField]
+    bool limitAcceleration = false;
+    [SerializeField, Min(0)]
+    float maxAcceleration = 100;
+    [SerializeField]
+    Vector3 minInitVelocity;
+    [SerializeField]
+    Vector3 maxInitVelocity;
+    Vector3 position;
+    Vector3 velocity;
+    Vector3 acceleration;
+    Transform thisTransform;
+
+    public GameObject obj;
+    Vector3 startPos;
+    private bool moveFlag = false;
+    public Transform Target
+    {
+        set
+        {
+            target = value;
+        }
+        get
+        {
+            return target;
+        }
+    }
+    void Start()
+    {
+        Debug.Log("Start");
+        thisTransform = transform;
+        position = thisTransform.position;
+        velocity = new Vector3(Random.Range(minInitVelocity.x, maxInitVelocity.x),
+            Random.Range(minInitVelocity.y, maxInitVelocity.y),
+            Random.Range(minInitVelocity.z, maxInitVelocity.z));
+        startPos = thisTransform.position;
+        time = 2.5f;
+        //StartCoroutine(nameof(Timer));
+        //target = GameObject.FindGameObjectWithTag("Target").transform;
+
+       // Instantiate(obj, startPos, Quaternion.identity);
+
+    }
+    public void Update()
+    {
+        Debug.Log("update");
+        //if (Input.GetKeyDown("space"))
+        //{
+
+        if (target == null)
+            {
+                return;
+            }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            moveFlag = true;
+            StartCoroutine(nameof(Timer));
+
+        }
+        if (moveFlag)
+        {
+            acceleration = 2f / (time * time) * (target.position - position - time * velocity);
+            if (limitAcceleration && acceleration.sqrMagnitude > maxAcceleration * maxAcceleration)
+            {
+                acceleration = acceleration.normalized * maxAcceleration;
+            }
+            time -= Time.deltaTime;
+            if (time < 0f)
+            {
+                return;
+            }
+            velocity += acceleration * Time.deltaTime;
+            position += velocity * Time.deltaTime;
+            thisTransform.position = position;
+            thisTransform.rotation = Quaternion.LookRotation(velocity);
+        }
+            
+        //}
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Target")
+        {
+            Destroy(obj, 0.5f);
+            Debug.Log("Destroy");
+        }
+    }
+
+    IEnumerator Timer()
+    {
+        //Instantiate(obj, startPos, Quaternion.identity);
+        yield return new WaitForSeconds(lifeTime);
+        //Destroy(this);
+        
+            Instantiate(obj, startPos, Quaternion.identity);
+            Destroy(this.gameObject);
+
+
+
+    }
+}
